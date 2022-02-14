b2b72f7aff25c6d1e573ca60974e72b4f7066eda
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-14 21:48:54 +0900

    Update 1_DNA.py

--- code/Bioinformatics-stronghold/1_DNA.py
+++ code/Bioinformatics-stronghold/1_DNA.py
@@ -1,9 +1,12 @@
 # rosalind_1_DNA
 # https://rosalind.info/problems/dna/
+#
+# Given: A DNA string s of length at most 1000 nt.
+# Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
 
 s = input()
 print(s.count('A'), s.count('C'), s.count('G'), s.count('T'))
 
-#참고할 풀이 *사용이 또 나왔네
+# Other's solution
 print(*map(input().count, "ACGT"))
 print(*(s.count(nuc) for nuc in 'ACGT'))
