2ce61207354acd02892880912b2d3c52033b3c9b
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-15 01:02:17 +0900

    Create 2_RNA.py

---
+++ code/Bioinformatics-stronghold/2_RNA.py
@@ -0,0 +1,12 @@
+# rosalind_1_RNA
+# https://rosalind.info/problems/rna/
+#
+# Given: A DNA string t having length at most 1000 nt.
+# The transcribed RNA string of t.
+
+#1
+print(input().replace('T', 'U'))
+
+#2
+print(input().translate(str.maketrans('T','U')))
+
