ef4c91e0ee0e113bec0ff02c9803a7c2a16c2e70
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-15 01:10:58 +0900

    Create 4_FIB.py

---
+++ code/Bioinformatics-stronghold/4_FIB.py
@@ -0,0 +1,7 @@
+# rosalind_4_FIB
+#
+# Rabbits and Recurrence Relations
+# https://rosalind.info/problems/fib/
+#
+# Given: Positive integers n≤40 and k≤5.
+# The total number of rabbit pairs that will be present after n months, if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k rabbit pairs (instead of only 1 pair).
