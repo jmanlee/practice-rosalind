b4c847ada20649fe13a9dec513f413dc10733e9b
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-15 14:17:04 +0900

    Create 8_PROT.py

---
+++ code/Bioinformatics-stronghold/8_PROT.py
@@ -0,0 +1,7 @@
+# rosalind_8_PROT
+#
+# Translating RNA into Protein
+# https://rosalind.info/problems/prot/
+#
+# Given: An RNA string s corresponding to a strand of mRNA (of length at most 10 kbp).
+# Return: The protein string encoded by s.
