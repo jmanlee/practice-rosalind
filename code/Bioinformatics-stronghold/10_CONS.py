b65bedc680478e550111297b156e1c11c31aecf8
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-15 21:04:18 +0900

    Create 10_CONS.py

---
+++ code/Bioinformatics-stronghold/10_CONS.py
@@ -0,0 +1,9 @@
+# rosalind_10_CONS
+#
+# Consensus and Profile
+# https://rosalind.info/problems/cons/
+#
+# Given: A collection of at most 10 DNA strings of equal length (at most 1 kbp) in FASTA format.
+# Return: A consensus string and profile matrix for the collection. (If several possible consensus strings exist, then you may return any one of them.)
+
+
