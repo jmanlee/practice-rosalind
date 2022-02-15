cc11ec56eb84de5ff6639a089bfd1d0e698327dc
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-15 20:19:20 +0900

    Create 9_SUBS.py

---
+++ code/Bioinformatics-stronghold/9_SUBS.py
@@ -0,0 +1,8 @@
+# rosalind_9_SUBS
+#
+# Finding a Motif in DNA
+# https://rosalind.info/problems/subs/
+#
+# Given: Two DNA strings s and t (each of length at most 1 kbp).
+# Return: All locations of t as a substring of s.
+
