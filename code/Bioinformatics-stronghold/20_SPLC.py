fb1676d576a8948064f85c7cdb505bbcc1ebddcf
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-16 22:18:32 +0900

    Create 20_SPLC.py

---
+++ code/Bioinformatics-stronghold/20_SPLC.py
@@ -0,0 +1,8 @@
+# rosalind_20_SPLC
+#
+# RNA Splicing
+# https://rosalind.info/problems/splc/
+#
+# Given: A DNA string s (of length at most 1 kbp) and a collection of substrings of s acting as introns. All strings are given in FASTA format.
+# Return: A protein string resulting from transcribing and translating the exons of s. (Note: Only one solution will exist for the dataset provided.)
+
