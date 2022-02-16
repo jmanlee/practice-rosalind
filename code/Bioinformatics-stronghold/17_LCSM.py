badcc853ca8a18306b2f2b20cb37edcdc297dc45
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-16 20:56:42 +0900

    Create 17_LCSM.py

---
+++ code/Bioinformatics-stronghold/17_LCSM.py
@@ -0,0 +1,8 @@
+# rosalind_17_LCSM
+#
+# Finding a Shared Motif
+# https://rosalind.info/problems/lcsm/
+#
+# Given: A collection of k (k≤100) DNA strings of length at most 1 kbp each in FASTA format.
+# Return: A longest common substring of the collection. (If multiple solutions exist, you may return any single solution.)
+
