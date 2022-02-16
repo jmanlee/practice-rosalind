3ff23803a4d8ba0208f136e15200d2b3135e9605
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-16 21:35:54 +0900

    Create 18_LIA.py

---
+++ code/Bioinformatics-stronghold/18_LIA.py
@@ -0,0 +1,9 @@
+# rosalind_18_LIA
+#
+# Transcribing DNA into RNA
+# Independent Alleles
+#
+# Given: Two positive integers k (k≤7) and N (N≤2k). In this problem, we begin with Tom, who in the 0th generation has genotype Aa Bb. 
+# Tom has two children in the 1st generation, each of whom has two children, and so on. Each organism always mates with an organism having genotype Aa Bb.
+# Return: The probability that at least N Aa Bb organisms will belong to the k-th generation of Tom's family tree (don't count the Aa Bb mates at each level). 
+# Assume that Mendel's second law holds for the factors.
