6cabb1e02e985cb5038ba420406a2ecf4302f26d
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-16 15:56:20 +0900

    Create 14_PROB.py

---
+++ code/Bioinformatics-stronghold/14_PROB.py
@@ -0,0 +1,11 @@
+# rosalind_14_PROB
+#
+# Introduction to Random Strings
+# https://rosalind.info/problems/prob/
+#
+# Given: A DNA string s of length at most 100 bp and an array A containing at most 20 numbers between 0 and 1.
+# Return: An array B having the same length as A in which B[k] represents the common logarithm of the probability 
+# that a random string constructed with the GC-content found in A[k] will match s exactly.
+
+
+
