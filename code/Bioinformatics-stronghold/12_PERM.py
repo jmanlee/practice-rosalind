23b38a48715999d65b774b283293570d3ef25b16
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-16 14:23:29 +0900

    Create 12_PERM.py

---
+++ code/Bioinformatics-stronghold/12_PERM.py
@@ -0,0 +1,8 @@
+# rosalind_12_PERM
+#
+# Enumerating Gene Orders
+# https://rosalind.info/problems/perm/
+#
+# Given: A positive integer n≤7.
+# Return: The total number of permutations of length n, followed by a list of all such permutations (in any order).
+
