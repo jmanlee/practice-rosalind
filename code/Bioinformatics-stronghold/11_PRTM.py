efa4244dc3188ac24d8bb640d0e73cb801c89cfc
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-16 14:17:12 +0900

    Create 11_PRTM.py

---
+++ code/Bioinformatics-stronghold/11_PRTM.py
@@ -0,0 +1,8 @@
+# rosalind_11_PRTM
+#
+# Calculating Protein Mass
+# https://rosalind.info/problems/prtm/
+#
+# Given: A protein string P of length at most 1000 aa.
+# Return: The total weight of P. Consult the monoisotopic mass table.
+
