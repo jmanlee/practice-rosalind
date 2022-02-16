a8789de9c7ee3b4380ea4271c9a9b84d01cec267
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-17 06:54:34 +0900

    Create 22_ORF.py

---
+++ code/Bioinformatics-stronghold/22_ORF.py
@@ -0,0 +1,8 @@
+# rosalind_22_ORF
+#
+# Open Reading Frames
+# https://rosalind.info/problems/orf/
+#
+# Given: A DNA string s of length at most 1 kbp in FASTA format.
+# Return: Every distinct candidate protein string that can be translated from ORFs of s. Strings can be returned in any order.
+
