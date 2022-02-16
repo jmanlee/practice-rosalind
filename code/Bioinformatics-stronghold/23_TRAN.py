c278705721053380d849e995374cabfa384ffd4d
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-17 07:13:51 +0900

    Create 23_TRAN.py

---
+++ code/Bioinformatics-stronghold/23_TRAN.py
@@ -0,0 +1,9 @@
+# rosalind_23_TRAN
+#
+# Transitions and Transversions
+# https://rosalind.info/problems/tran/
+#
+# Given: Two DNA strings s1 and s2 of equal length (at most 1 kbp).
+# Return: The transition/transversion ratio R(s1,s2).
+
+
