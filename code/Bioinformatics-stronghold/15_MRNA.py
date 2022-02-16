befc130d994fe8716956e6e5df7ed19e96764b26
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-16 20:06:32 +0900

    Create 15_MRNA.py

---
+++ code/Bioinformatics-stronghold/15_MRNA.py
@@ -0,0 +1,9 @@
+# rosalind_15_MRNA
+#
+# Inferring mRNA from Protein
+# https://rosalind.info/problems/mrna/
+#
+# Given: A protein string of length at most 1000 aa.
+# Return: The total number of different RNA strings from which the protein could have been translated, modulo 1,000,000. 
+# (Don't neglect the importance of the stop codon in protein translation.)
+
