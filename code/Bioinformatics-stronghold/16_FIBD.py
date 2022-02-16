8d57ac011cd040a6080479d864d423842b8a8479
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-16 20:52:30 +0900

    Update 16_FIBD.py

--- code/Bioinformatics-stronghold/16_FIBD.py
+++ code/Bioinformatics-stronghold/16_FIBD.py
@@ -1,6 +1,11 @@
-
-
 # rosalind_16_FIBD
+#
+# Mortal Fibonacci Rabbits
+# https://rosalind.info/problems/fibd/
+#
+# Given: Positive integers n≤100 and m≤20.
+# Return: The total number of pairs of rabbits that will remain after the n-th month if all rabbits live for m months.
+
 
 if __name__ == "__main__":
 
