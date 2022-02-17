07dafe8c94895f2076e4b6fa410c8a10639019b0
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-17 09:23:15 +0900

    Create 26_PMCH.py

---
+++ code/Bioinformatics-stronghold/26_PMCH.py
@@ -0,0 +1,7 @@
+# rosalind_26_PMCH
+#
+# Perfect Matchings and RNA Secondary Structures
+# https://rosalind.info/problems/pmch/
+#
+# Given: An RNA string s of length at most 80 bp having the same number of occurrences of 'A' as 'U' and the same number of occurrences of 'C' as 'G'.
+# Return: The total possible number of perfect matchings of basepair edges in the bonding graph of s.
