607d9380f981260e8a512b5ae4ea041b3f5b38e9
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-18 03:13:28 +0900

    Update 25_LEXF.py

--- code/Bioinformatics-stronghold/25_LEXF.py
+++ code/Bioinformatics-stronghold/25_LEXF.py
@@ -6,16 +6,18 @@
 # Given: A collection of at most 10 symbols defining an ordered alphabet, and a positive integer n (n≤10).
 # Return: All strings of length n that can be formed from the alphabet, ordered lexicographically (use the standard order of symbols in the English alphabet).
 
+#0) refer 12_PERM 
+
 def lexf1(alpha_list, N):
     
     result = []
     
-    if N == 0:  ### N == 1, return alpha_list 하면 더 낫다.
-        return [[]] # [[]]로 해야, 다음 구문에서 for 에서 none이 안나옴.
+    if N == 0:      ### N == 1, return alpha_list 하면 더 낫다.
+        return [[]]     # [[]]로 해야, 다음 구문에서 for 에서 none이 안나옴.
     
-    for alpha in alpha_list:    # 'a', 'b', 'c'
-        result.extend( [alpha] + word for word in lexf1(alpha_list, N-1) ) #전체를 안 둘러쌓도, extend가 되네.. ['a', 'b', 'c'] 식
-    
+    for alpha in alpha_list:    # 'a', 'b', 'c' // [ ['a', 'a'], ['a', 'b'], ...]
+        result.extend( [alpha] + word for word in lexf1(alpha_list, N-1) )
+        
     return result
 
 def lexf2(alpha_list, N):
@@ -23,14 +25,14 @@ def lexf2(alpha_list, N):
     result = []
     
     if N == 1: 
-        return alpha_list # 이렇게 하면, 위에 꺼랑 달리 str concatenation으로 해결 가능.
+        return alpha_list   # 이렇게 하면, 위에 꺼랑 달리 str concatenation으로 해결 가능.
     
     for alpha in alpha_list:    # 'a', 'b', 'c' // ['aa', 'ab' ...]
-        result.extend( [alpha + word for word in lexf2(alpha_list, N-1)] ) #여기선 list comprehesion을 잘 보이기 위해 전체를 []로 둘러싸자.
+        result.extend( [alpha + word for word in lexf2(alpha_list, N-1)] )
     
     return result
-        
-        
+
+
 if __name__ == '__main__':
     
     with open('./datasets/rosalind_lexf.txt') as inFile:
@@ -40,7 +42,24 @@ if __name__ == '__main__':
         
     with open('./answers/rosalind_lexf_outFile.txt', 'w') as outFile:
         
-        word_list = lexf1(alphabet, N) # ['aa', 'ab', ..]
+        word_list = lexf1(alphabet, N)  # ['aa', 'ab', ..]
         for word in word_list: 
             print(''.join(word), file = outFile) 
+        
+'''other's solution
+
+for p in it.product(alphabet,repeat=n):
+    print ''.join(p)
+    
+k = 2
+str = 'P R Q Z U A S N J D'
+alphabet = str.split()
+k_mer = alphabet
+
+for  l in range(k-1):
+    k_mer =  [i+j for i in alphabet for j in k_mer]
+
+for i in k_mer: print i
+'''
+    
         
