506536953487551b82afa31d9a91dcc97223fe6b
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-17 14:47:33 +0900

    Create 25_LEXF.py

---
+++ code/Bioinformatics-stronghold/25_LEXF.py
@@ -0,0 +1,9 @@
+# rosalind_25_LEXF
+#
+# Enumerating k-mers Lexicographically
+# https://rosalind.info/problems/lexf/
+#
+# Given: A collection of at most 10 symbols defining an ordered alphabet, and a positive integer n (n≤10).
+# Return: All strings of length n that can be formed from the alphabet, ordered lexicographically (use the standard order of symbols in the English alphabet).
+
+
