5aef8b589e213e368103f0d67d20a8503972ad1e
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-17 10:55:04 +0900

    Create 24_REVP.py

---
+++ code/Bioinformatics-stronghold/24_REVP.py
@@ -0,0 +1,8 @@
+# rosalind_24_REVP
+#
+# Locating Restriction Sites
+# https://rosalind.info/problems/revp/
+#
+# Given: A DNA string of length at most 1 kbp in FASTA format.
+# Return: The position and length of every reverse palindrome in the string having length between 4 and 12. You may return these pairs in any order.
+
