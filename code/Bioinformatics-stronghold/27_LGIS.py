53632053ae1d103fe8794e0ed22957d8135e5c6c
Jaeman Lee <jaemann.lee@gmail.com>
2022-02-18 07:28:02 +0900

    Update 27_LGIS.py

--- code/Bioinformatics-stronghold/27_LGIS.py
+++ code/Bioinformatics-stronghold/27_LGIS.py
@@ -6,7 +6,7 @@
 # Given: A positive integer n≤10000 followed by a permutation π of length n.
 # Return: A longest increasing subsequence of π, followed by a longest decreasing subsequence of π.
 
-# Dynamic programming...?
+# Dynamic programming?
 # 1. 모든 증가 하는 부분 수열의 경우의 수를 구해서 max(list, key = len?)
 # 2. n개의 개수를 갖는 sub_seq을 하나씩만 유지한다. 이 때, 가장 마지막값을 가장 작은 값이 되도록 업데이트한다.
 
@@ -38,7 +38,7 @@ def LGIS_2(perm: list):  # Longest Increasing Subsequence
         for i in range(len(result)):
             if i == 0:  # first seq
                 cache.append(min([result[i], [num]], key=lambda x: x[-1]))
-            else:  # i > 1
+            else:  # i >= 1
                 if result[i - 1][-1] < num:  # update_possible
                     cache.append(min([result[i], result[i - 1] + [num]], key=lambda x: x[-1])                    )
                 else:  # unpdate_impossible
@@ -65,7 +65,7 @@ def LGDS_2(perm: list):  # Longest Decreasing Subsequence
         for i in range(len(result)):
             if i == 0:  # first seq
                 cache.append(max([result[i], [num]], key=lambda x: x[-1]))
-            else:  # i > 1
+            else:  # i >= 1
                 if result[i - 1][-1] > num:  # update_possible
                     cache.append(max([result[i], result[i - 1] + [num]], key=lambda x: x[-1])                    )
                 else:  # unpdate_impossible
@@ -77,17 +77,6 @@ def LGDS_2(perm: list):  # Longest Decreasing Subsequence
         result = cache
 
     return result
-
-#others solution
-def solution(): #???? how...?
-    
-    inc = [(0,[])]*(n+1)
-    l = []
-    for i in l:
-        x,y = max(inc[:i])
-        inc[i] = (x+1,y+[i])
-
-    print(" ".join(map(str,max(inc)[1])))
 
 
 if __name__ == "__main__":
@@ -103,4 +92,17 @@ if __name__ == "__main__":
 
         print(' '.join(map(str, lgis_seqs[-1])), file=outFile)        
         print(' '.join(map(str, lgds_seqs[-1])), file=outFile)
+
+        
+#others solution
+def solution(): #???? how...?
+    
+    inc = [(0,[])]*(n+1)
+    l = []
+    for i in l:
+        x,y = max(inc[:i])
+        inc[i] = (x+1,y+[i])
+
+    print(" ".join(map(str,max(inc)[1])))
+
 
